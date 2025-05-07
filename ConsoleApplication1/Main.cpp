8dff4b2e51f4555ac1da9039403f212d75fd68b9
poula00saber <bola00saber@gmail.com>
2025-05-07 19:54:12 +0300

    debugging bfs

--- ConsoleApplication1/Main.cpp
+++ ConsoleApplication1/Main.cpp
@@ -9,10 +9,12 @@
 #include <SFML/Audio.hpp>
 #include <sstream>
 #include <iomanip>
+#include "Graph.cpp"
 using namespace std;
 using namespace sf;
 
-
+unordered_map<int, vector<int>> Graph::graph;
+unordered_map<int, Graph::Node> Graph::nodesInfo;
 sf::RenderWindow window(sf::VideoMode(1680, 1050), "SFML works!");
 sf::CircleShape shape(100.f);
 void Update();
@@ -21,6 +23,78 @@ void Draw();
 float deltaTime;
 int main()
 {
+   
+	Graph g;
+	int row = g.pacmanMatrix.size();
+	int col = g.pacmanMatrix[0].size();
+	for (int i = 0; i < row; i++)
+	{
+		for (int j = 0; j < col; j++)
+		{
+            int x = i * col + j;
+            if (x<10)
+            {
+			cout <<x << "   ";
+            continue;
+            }
+			if (x < 100)
+			{
+				cout << x << "  ";
+				continue;
+			}
+			if (x < 1000)
+			{
+				cout << x << " ";
+				continue;
+			}
+
+
+		}
+		cout << endl;
+	}
+	cout << endl;
+	cout << endl;
+
+
+
+	for (int i = 0; i < row; i++)
+	{
+		for (int j = 0; j < col; j++)
+		{
+			int x = g.pacmanMatrix[i][j];
+			if (x < 10)
+			{
+				cout << x << "   ";
+				continue;
+			}
+			if (x < 100)
+			{
+				cout << x << "  ";
+				continue;
+			}
+			if (x < 1000)
+			{
+				cout << x << " ";
+				continue;
+			}
+
+
+		}
+		cout << endl;
+	}
+
+
+
+		g.graph = g.constructGraph(g.pacmanMatrix, 90);
+
+	vector<int> path = g.bfs(22, 25);
+
+	cout << path.size() << endl << endl << endl << endl;
+	for (int i = 0; i < path.size(); i++)
+	{
+		cout << path[i] << " ";
+	}
+
     Start();
     Clock clock;
     while (window.isOpen())
